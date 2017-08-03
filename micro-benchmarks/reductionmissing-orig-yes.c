1a4991ebbe009faa9f4fe9f8c23fe9340bfa4c59
Markus Schordan <schordan1@llnl.gov>
2017-08-03 10:23:56 -0700

    Added initialization of array 'u'. Renamed variable 'error' to 'sum'. Added initialization of variable 'sum'.

--- micro-benchmarks/reductionmissing-orig-yes.c
+++ micro-benchmarks/reductionmissing-orig-yes.c
@@ -47,23 +47,28 @@ THE POSSIBILITY OF SUCH DAMAGE.
 
 /*
  A kernel for two level parallelizable loop with reduction 
- if reduction(error) is missing, there is race condition.
+ if reduction(+:sum) is missing, there is race condition.
 */
 #include <stdio.h>
 int main(int argc, char* argv[])
 {
   int i,j;
-  float temp, error;
+  float temp, sum=0.0;
   int len=100;
 
-   float u[100][100];
+  float u[100][100];
+  for (i = 0; i < len; i++)
+    for (j = 0; j < len; j++)
+        u[i][j] = 0.5; 
+
 #pragma omp parallel for private (temp,i,j)
   for (i = 0; i < len; i++)
     for (j = 0; j < len; j++)
     {
       temp = u[i][j];
-      error = error + temp * temp;
+      sum = sum + temp * temp;
     }
-  printf ("error = %f\n", error);  
+
+  printf ("sum = %f\n", sum);  
   return 0;
 }

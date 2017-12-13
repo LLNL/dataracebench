2493459eee024f1b080bb7b9b26f9e37cff8fd11
Chunhua (Leo) Liao <liaoch@llnl.gov>
2017-12-13 15:48:11 -0800

    Updated the PI calculation test * correct the formula * output the final result

--- micro-benchmarks/pireduction-orig-no.c
+++ micro-benchmarks/pireduction-orig-no.c
@@ -49,16 +49,22 @@ Classic PI calculation using reduction
 */
 
 #define num_steps 2000000000 
-    
-int main(int argc, char** argv) 
+
+#include <stdio.h>
+int main(int argc, char** argv)
 {
-  double pi = 0;
+  double pi = 0.0;
   int i;
-#pragma omp parallel for reduction(+:pi)
+  double x, interval_width;
+  interval_width = 1.0/(double)num_steps;
+
+#pragma omp parallel for reduction(+:pi) private(x)
   for (i = 0; i < num_steps; i++) {
-    pi += 1.0 / (i * 4.0 + 1.0);
+    x = (i+ 0.5) * interval_width;
+    pi += 1.0 / (x*x + 1.0);
   }
-  pi = pi * 4.0;
+
+  pi = pi * 4.0 * interval_width;
+  printf ("PI=%f\n", pi);
   return 0;
 }
-

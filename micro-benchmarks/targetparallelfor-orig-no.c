bdbce91a2e22a7d0b41f7eeb88466165c934401d
Chunhua (Leo) Liao <liao6@llnl.gov>
2018-04-17 11:28:24 -0700

    minor fix

--- micro-benchmarks/targetparallelfor-orig-no.c
+++ micro-benchmarks/targetparallelfor-orig-no.c
@@ -45,21 +45,20 @@ THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 /*
-use of omp target
+use of omp target: len is not mapped. It should be firstprivate within target.
 */
 int main(int argc, char* argv[])
 {
   int i;
   int len = 1000;
-
-  int a[1000];
+  int a[len];
 
   for (i=0; i<len; i++)
     a[i]= i;
 
 #pragma omp target map(a[0:len])
 #pragma omp parallel for
-  for (i=0;i< len -1 ;i++)
+  for (i=0;i< len;i++)
     a[i]=a[i]+1;
 
   return 0;

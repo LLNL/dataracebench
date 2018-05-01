a675ae1c6709e67aef683b81d574eb36ce3963bd
Chunhua (Leo) Liao <liao6@llnl.gov>
2018-05-01 15:50:35 -0700

    replace new with nnew so c++ compilers can be used

--- micro-benchmarks/utilities/polybench.c
+++ micro-benchmarks/utilities/polybench.c
@@ -381,14 +381,14 @@ static
 void *
 xmalloc (size_t num)
 {
-  void* new = NULL;
-  int ret = posix_memalign (&new, 32, num);
-  if (! new || ret)
+  void* nnew = NULL;
+  int ret = posix_memalign (&nnew, 32, num);
+  if (! nnew || ret)
     {
       fprintf (stderr, "[PolyBench] posix_memalign: cannot allocate memory");
       exit (1);
     }
-  return new;
+  return nnew;
 }
 
 

aa9dcdca4d2c163469978743ea154027a30bb091
xintin <gaurav.verma@stonybrook.edu>
2020-06-10 16:12:12 -0400

    Adding micro-benchmarks in F95 50-54 and 57-59

---
+++ micro-benchmarks-fortran/DRB053-inneronly1-orig-no.f95
@@ -0,0 +1,34 @@
+!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!
+!!! Copyright (c) 2017-20, Lawrence Livermore National Security, LLC
+!!! and DataRaceBench project contributors. See the DataRaceBench/COPYRIGHT file for details.
+!!!
+!!! SPDX-License-Identifier: (BSD-3-Clause)
+!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!
+
+!Example with loop-carried data dependence at the outer level loop.
+!But the inner level loop can be parallelized.
+
+
+program DRB053_inneronly1_orig_no
+    use omp_lib
+    implicit none
+
+    integer :: i, j
+
+    real, dimension(:,:), allocatable :: a
+    allocate (a(20,20))
+
+    do i = 1, 20
+        do j = 1, 20
+            a(i,j) = 0.0
+        end do
+    end do
+
+    do i = 1, 19
+    !$omp parallel do
+        do j = 1, 20
+            a(i,j) = a(i,j)+a(i+1,j)
+        end do
+    !$omp end parallel do
+    end do
+end program
---
+++ micro-benchmarks-fortran/DRB054-inneronly2-orig-no.f95
@@ -0,0 +1,37 @@
+!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!
+!!! Copyright (c) 2017-20, Lawrence Livermore National Security, LLC
+!!! and DataRaceBench project contributors. See the DataRaceBench/COPYRIGHT file for details.
+!!!
+!!! SPDX-License-Identifier: (BSD-3-Clause)
+!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!
+
+!Example with loop-carried data dependence at the outer level loop.
+!The inner level loop can be parallelized.
+
+program DRB054_inneronly2_orig_no
+    use omp_lib
+    implicit none
+
+    integer i,j,n,m
+    real, dimension(:,:), allocatable :: b
+
+    n = 100
+    m = 100
+
+    allocate (b(n,m))
+
+    do i = 1, n
+        do j = 1, m
+            b(i,j) = i*j
+        end do
+    end do
+
+    do i = 2, n
+        !$omp parallel do
+        do j =2, m
+            b(i,j)=b(i-1,j-1)
+        end do
+        !$omp end parallel do
+    end do
+
+end program

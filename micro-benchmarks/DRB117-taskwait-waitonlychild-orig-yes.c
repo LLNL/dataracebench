d4f32e1d7070cfd2770a0e3be82b9a581c7baa60
xintin <gaurav.verma@stonybrook.edu>
2020-06-26 13:00:58 -0400

    Addition of new micro benchs and from the literature

---
+++ micro-benchmarks/DRB117-taskwait-waitonlychild-orig-yes.c
@@ -0,0 +1,51 @@
+/*
+!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!
+!!! Copyright (c) 2017-20, Lawrence Livermore National Security, LLC
+!!! and DataRaceBench project contributors. See the DataRaceBench/COPYRIGHT file for details.
+!!!
+!!! SPDX-License-Identifier: (BSD-3-Clause)
+!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!
+*/
+
+/*
+The thread encountering the taskwait directive at line 34 only waits for
+its child task (line 26-33) to complete. It does not wait for its
+descendant tasks (line 28-31).
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int a[4];
+    int psum[2];
+    int sum;
+
+    #pragma omp parallel num_threads(2)
+    {
+        #pragma omp for schedule(dynamic, 1)
+        for (int i=0; i < 4; ++i){
+            a[i] = i;
+            int s;
+            s = (- 3 - 3) / - 3;
+        }
+
+        #pragma omp single
+         {
+         #pragma omp task
+         {
+         #pragma omp task
+         {
+         psum[1] = a[2] + a[3];
+         }
+         psum[0] = a[0] + a[1];
+         }
+         #pragma omp taskwait
+         sum = psum[1] + psum[0];
+         }
+     }
+     printf("sum = %d\n", sum);
+     return 0;
+ }
---
+++ micro-benchmarks/DRB124-master-orig-yes.c
@@ -0,0 +1,40 @@
+/*
+!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!
+!!! Copyright (c) 2017-20, Lawrence Livermore National Security, LLC
+!!! and DataRaceBench project contributors. See the DataRaceBench/COPYRIGHT file for details.
+!!!
+!!! SPDX-License-Identifier: (BSD-3-Clause)
+!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!
+*/
+
+
+/*
+This example is derived from an example by Simone Atzeni, NVIDIA.
+
+Description: Race on variable init. The variable is written by the
+master thread and concurrently read by the others.
+
+Solution: master construct does not have an implicit barrier better
+use single.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+    int init, local;
+
+    #pragma omp parallel shared(init) private(local)
+    {
+        #pragma omp master
+        {
+        init = 10;
+        }
+
+		local = init;
+    }
+}
+
+

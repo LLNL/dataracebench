d9911c1e726e81933f127f3045fd94fd99292259
xintin <gaurav.verma@stonybrook.edu>
2020-07-14 21:44:29 -0400

    GPU Micro-Specs

---
+++ micro-benchmarks/DRB147-gpu-critical1-orig-no.c
@@ -0,0 +1,37 @@
+/*
+!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!
+!!! Copyright (c) 2017-20, Lawrence Livermore National Security, LLC
+!!! and DataRaceBench project contributors. See the DataRaceBench/COPYRIGHT file for details.
+!!!
+!!! SPDX-License-Identifier: (BSD-3-Clause)
+!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!
+ */
+
+/*
+Concurrent access on same variable var@29 and var@32 leads to the race condition if two different
+locks are used. This is the reason here we have used the critical directive to ensure that addition
+and subtraction are not interleaved. No data race pairs.
+*/
+
+#include <omp.h>
+#include <stdio.h>
+
+#define N 100
+
+int var = 0;
+
+int main(){
+
+  #pragma omp target map(tofrom:var) device(0)
+  #pragma omp teams distribute parallel for
+  for(int i=0; i<N; i++){
+    #pragma omp critical
+    var++;
+
+    #pragma omp critical
+    var = var - 2;
+  }
+
+  printf("%d\n",var);
+  return 0;
+}
---
+++ micro-benchmarks/DRB148-gpu-critical1-orig-yes.c
@@ -0,0 +1,40 @@
+/*
+!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!
+!!! Copyright (c) 2017-20, Lawrence Livermore National Security, LLC
+!!! and DataRaceBench project contributors. See the DataRaceBench/COPYRIGHT file for details.
+!!!
+!!! SPDX-License-Identifier: (BSD-3-Clause)
+!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!
+ */
+
+/*
+This example is referred from DataRaceOnAccelerator : A Micro-benchmark Suite for Evaluating
+Correctness Tools Targeting Accelerators.
+Though we have used critical directive to ensure that addition and subtraction are not overlapped,
+due to different locks addlock@30:26 and sublock@33:26 interleave each others operation.
+Data Race pairs, var@31:5 - var@31:5 and var@34:5 - var@34:11
+*/
+
+#include <omp.h>
+#include <stdio.h>
+
+#define N 100
+
+int var = 0;
+
+int main(){
+
+  #pragma omp target map(tofrom:var) device(0)
+  #pragma omp teams distribute parallel for
+  for(int i=0; i<N; i++){
+    #pragma omp critical(addlock)
+    var++;
+
+    #pragma omp critical(sublock)
+    var = var-2;
+  }
+
+  printf("%d\n",var);
+
+  return 0;
+}

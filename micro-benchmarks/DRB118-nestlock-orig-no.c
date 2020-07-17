a859a3ad47029793cb9d4fd6c426872b50bf14f9
Joachim Protze <protze@itc.rwth-aachen.de>
2020-07-17 13:08:33 +0200

    Several fixes for DRB118/DRB119
    Allocate `pair`
    Initialize a/b
    Remove unused variables
    Fix return type of the functions
    Minimize diff of -no and -yes code

--- micro-benchmarks/DRB118-nestlock-orig-no.c
+++ micro-benchmarks/DRB118-nestlock-orig-no.c
@@ -13,7 +13,6 @@ it as many times as the number of calls to omp_set_nest_lock.
 incr_b is called at line 49 and line 54. So, it needs a nest_lock at line 30-32
 */
 
-
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,10 +22,10 @@ typedef struct {
   omp_nest_lock_t lck;
 } pair;
 
-int incr_a(pair *p){
+void incr_a(pair *p){
   p->a += 1;
 }
-int incr_b(pair *p){
+void incr_b(pair *p){
   omp_set_nest_lock(&p->lck);
     p->b += 1;
   omp_unset_nest_lock(&p->lck);
@@ -35,29 +34,27 @@ int incr_b(pair *p){
 
 int main(int argc, char* argv[])
 {
-  int var1=0, var2=0;
-  pair *p;
+  pair p[1];
+  p->a = 0;
+  p->b = 0;
   omp_init_nest_lock(&p->lck);
 
-  #pragma omp parallel shared (var1, var2)
+  #pragma omp parallel sections
   {
-    #pragma omp parallel sections
-    {
     #pragma omp section
     {
-    omp_set_nest_lock(&p->lck);
-    incr_b(p);
-    incr_a(p);
-    omp_unset_nest_lock(&p->lck);
+      omp_set_nest_lock(&p->lck);
+      incr_b(p);
+      incr_a(p);
+      omp_unset_nest_lock(&p->lck);
     }
     #pragma omp section
-    incr_b(p);
-    }
+      incr_b(p);
   }
 
   omp_destroy_nest_lock(&p->lck);
 
-  int error = (p->a != ((p->b)/2)+1);
+  int error = (p->a != ((p->b)/2));
   printf("%d\n",p->b);
   return error;
 }

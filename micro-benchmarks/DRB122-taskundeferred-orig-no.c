a9f84b3e2580282bf4fe247f11d10e8e2ce33ecf
xintin <gaurav.verma@stonybrook.edu>
2020-07-17 13:56:17 -0400

    updated correct if(0) usage and parallel sections based on review

--- micro-benchmarks/DRB122-taskundeferred-orig-no.c
+++ micro-benchmarks/DRB122-taskundeferred-orig-no.c
@@ -9,7 +9,7 @@
 
 /*
 No data race. The data environment of the task is created according to the
-data-sharing attribute clauses, here at line 27 it is var. Hence, var is
+data-sharing attribute clauses, here at line 28 it is var. Hence, var is
 modified 10 times, resulting to the value 10.
 */
 
@@ -22,14 +22,16 @@ int main(int argc, char* argv[])
   int var = 0;
   int i;
 
-  #pragma omp parallel for shared(var) schedule(static,1)
-  for (i = 0; i < 10; i++) {
-    #pragma omp task shared(var)
-    {
-      var++;
+  #pragma omp parallel sections 
+  {
+    for (i = 0; i < 10; i++) {
+      #pragma omp task shared(var) if(0)
+      {
+        var++;
+      }
     }
   }
 
-  printf("%d \n",var);
+  printf("%d\n",var);
   return 0;
 }

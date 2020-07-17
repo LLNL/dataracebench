2eae69de04a41e80f8f01d8268f7aa27b120de2c
xintin <gaurav.verma@stonybrook.edu>
2020-07-17 12:12:23 -0400

    Update based on review comments

--- micro-benchmarks/DRB126-firstprivatesections-orig-no.c
+++ micro-benchmarks/DRB126-firstprivatesections-orig-no.c
@@ -11,8 +11,7 @@
  * This example is based on fpriv_sections.1.c OpenMP Examples 5.0.0
  * The section construct modifies the value of section_count which breaks the independence of the
  * section constructs. If the same thread executes both the section one will print 1 and the other
- * will print 2. The order is unspecified.
- * Data Race Pairs, section_count@30:7 and section_count@35:7. It may print 1 or 2.
+ * will print 2. For a same thread execution, there is no data race. 
  */
 
 #include <omp.h>
@@ -21,6 +20,8 @@
 int main(){
   int section_count = 0;
   omp_set_dynamic(0);
+  
+  omp_set_num_threads(1);
 
   #pragma omp parallel
   #pragma omp sections firstprivate( section_count )

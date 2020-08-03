c6e20c8688a4aeb3f356c120c02387872d674760
Chunhua "Leo" Liao <liao6@llnl.gov>
2020-08-03 16:43:19 -0700

    Update DRB174-critical-orig-no.c

--- micro-benchmarks/DRB174-critical-orig-no.c
+++ micro-benchmarks/DRB174-critical-orig-no.c
@@ -7,25 +7,25 @@
 !!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!
  */
 /**
- * FT.C: This file is part of kernel of the NAS Parallel Benchmarks 3.0 FT suit.
- * Intel Inspector can not correctly analyze, and report a false positive.
- * Tsan can not correctly analyzel, and report a false positive.
+ * FT.C: This file is part of kernel of the NAS Parallel Benchmarks 3.0's FT.
+ * Intel Inspector reports a false positive.
+ * Tsan reports a false positive.
 */
 
 #include <stdio.h>
 
-int main(int argh, char* argv[]){
+int main(int argc, char* argv[]){
   int i;
   double q[10], qq[10];
 
-  #pragma omp parallel for default(shared) private(i)
   for (i = 0; i < 10; i++) qq[i] = 1.0;
   for (i = 0; i < 10; i++) q[i] = 0.0;
   /*initial m */
   #pragma omp parallel default(shared) 
   {
     #pragma omp for //intel
-      for (i = 0; i <= 10 - 1; i++) q[i] += qq[i];
+      for (i = 0; i <= 10 - 1; i++) 
+        q[i] += qq[i];
     #pragma omp critical //tsan
       {
           q[i] = 1.0;
@@ -35,7 +35,6 @@ int main(int argh, char* argv[]){
      {
         q[i] = q[i] - 1.0;
      }
-
   }
   return 0;
 }

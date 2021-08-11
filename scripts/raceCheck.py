3d8c756f3981cb61481531db47a8ef13fd75d9f3
Joachim Protze <protze@itc.rwth-aachen.de>
2021-08-11 14:21:31 +0200

    Fix logic in raceCheck

--- scripts/raceCheck.py
+++ scripts/raceCheck.py
@@ -10,6 +10,8 @@ REF_DIR="references"
 CDIR="C"
 FDIR="Fortran"
 
+TP,FP = (0,1)
+
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 200)
@@ -19,6 +21,12 @@ def matchLineAndCol(gt, tool, id1, id2, r1, r2):
 	        #   tool reported column is in  the annotation range  ( column , column + len(dataname) + 1 )
 		int(tool.loc[id2, r1+"_column"]) in range(int(gt.loc[id1, r2+"_column"]), int(gt.loc[id1, r2+"_column"])+len(gt.loc[id1, r2+"_dataname"])+2))
 
+def evalTFP(vals):
+	if vals[FP]:
+		return "FP"
+	if vals[TP]:
+		return "TP"
+	return "FN"
 
 def queryandcompare(argv):
 	#print(argv[0])
@@ -39,24 +47,45 @@ def queryandcompare(argv):
 
 	# sourceRace['raceTypeMatch?'] = np.where((sourceRace['ref1_type'] == toolReport['ref1_type']) & (sourceRace['ref2_type'] == toolReport['ref2_type']), 'True', 'False')  #create new column in df1 to check if prices match
 	
-	for id1 in sourceRace.index:
-		typematched = False
-		linematched = False
-		colmatched  = False
-		for id2 in toolReport.index:
-			typematched = (sourceRace.loc[id1, "ref1_type"] ==  toolReport.loc[id2, "ref1_type"]) & (sourceRace.loc[id1, "ref2_type"] ==  toolReport.loc[id2, "ref2_type"])
-			if( typematched & (sourceRace.loc[id1, "ref2_type"] == 'R')):
-				linematched = (sourceRace.loc[id1, "ref1_line"] ==  toolReport.loc[id2, "ref1_line"]) & (sourceRace.loc[id1, "ref2_line"] ==  toolReport.loc[id2, "ref2_line"])
-				colmatched = linematched & ((matchLineAndCol(sourceRace, toolReport, id1, id2, "ref1", "ref1") and
-							     matchLineAndCol(sourceRace, toolReport, id1, id2, "ref2", "ref2")))
-			elif( typematched & (sourceRace.loc[id1, "ref2_type"] == 'W')):
-				linematched = (((sourceRace.loc[id1, "ref1_line"] ==  toolReport.loc[id2, "ref1_line"]) & (sourceRace.loc[id1, "ref2_line"] ==  toolReport.loc[id2, "ref2_line"])) | ((sourceRace.loc[id1, "ref1_line"] ==  toolReport.loc[id2, "ref2_line"]) & (sourceRace.loc[id1, "ref2_line"] ==  toolReport.loc[id2, "ref1_line"])))
-				colmatched = linematched & ((matchLineAndCol(sourceRace, toolReport, id1, id2, "ref1", "ref1") and
-							     matchLineAndCol(sourceRace, toolReport, id1, id2, "ref2", "ref2")) or
-							    (matchLineAndCol(sourceRace, toolReport, id1, id2, "ref1", "ref2") and
-                                                             matchLineAndCol(sourceRace, toolReport, id1, id2, "ref2", "ref1")))
+	typeMatchResult = [False,False]
+	lineMatchResult = [False,False]
+	colMatchResult = [False,False]
+	
 
-		print(typematched, linematched, colmatched) 
+	if(len(toolReport)>0):
+		for id2 in toolReport.index:
+			typematchedFP = True
+			linematchedFP = True
+			colmatchedFP  = True
+			for id1 in sourceRace.index:
+				colmatched = linematched = typematched = (sourceRace.loc[id1, "ref1_type"] ==  toolReport.loc[id2, "ref1_type"]) & (sourceRace.loc[id1, "ref2_type"] ==  toolReport.loc[id2, "ref2_type"])
+				if (typematched):
+					typematchedFP = False
+					typeMatchResult[TP] = True
+				if( typematched & (sourceRace.loc[id1, "ref2_type"] == 'R')):
+					linematched = (sourceRace.loc[id1, "ref1_line"] ==  toolReport.loc[id2, "ref1_line"]) & (sourceRace.loc[id1, "ref2_line"] ==  toolReport.loc[id2, "ref2_line"])
+					colmatched = linematched & ((matchLineAndCol(sourceRace, toolReport, id1, id2, "ref1", "ref1") and
+								     matchLineAndCol(sourceRace, toolReport, id1, id2, "ref2", "ref2")))
+				elif( typematched & (sourceRace.loc[id1, "ref2_type"] == 'W')):
+					linematched = (((sourceRace.loc[id1, "ref1_line"] ==  toolReport.loc[id2, "ref1_line"]) & (sourceRace.loc[id1, "ref2_line"] ==  toolReport.loc[id2, "ref2_line"])) | ((sourceRace.loc[id1, "ref1_line"] ==  toolReport.loc[id2, "ref2_line"]) & (sourceRace.loc[id1, "ref2_line"] ==  toolReport.loc[id2, "ref1_line"])))
+					colmatched = linematched & ((matchLineAndCol(sourceRace, toolReport, id1, id2, "ref1", "ref1") and
+								     matchLineAndCol(sourceRace, toolReport, id1, id2, "ref2", "ref2")) or
+								    (matchLineAndCol(sourceRace, toolReport, id1, id2, "ref1", "ref2") and
+								     matchLineAndCol(sourceRace, toolReport, id1, id2, "ref2", "ref1")))
+				if (linematched):
+					linematchedFP = False
+					lineMatchResult[TP] = True
+				if (colmatched):
+					colmatchedFP = False
+					colMatchResult[TP] = True
+			if(typematchedFP):
+				typeMatchResult[FP]=True
+			if(linematchedFP):
+				lineMatchResult[FP]=True
+			if(colmatchedFP):
+				colMatchResult[FP]=True
+
+	print(evalTFP(typeMatchResult), evalTFP(lineMatchResult),evalTFP(colMatchResult)) 
 
 if __name__ == "__main__":
 	queryandcompare(sys.argv[1:])

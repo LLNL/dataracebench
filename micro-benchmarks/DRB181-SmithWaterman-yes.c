1c465457696ffa48a2d4f608ddc81c882b8497ff
Pei-Hung Lin <lin32@llnl.gov>
2021-09-20 09:30:40 -0700

    added DRB181-SmithWaterman-yes.c

---
+++ micro-benchmarks/DRB181-SmithWaterman-yes.c
@@ -0,0 +1,259 @@
+/*
+!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!
+!!! Copyright (c) 2017-20, Lawrence Livermore National Security, LLC
+!!! and DataRaceBench project contributors. See the DataRaceBench/COPYRIGHT file
+for details.
+!!!
+!!! SPDX-License-Identifier: (BSD-3-Clause)
+!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!
+ */
+
+/* 
+   Data Race Pair, *maxPos@179:9:W vs. *maxPos@177:17:R
+                   H[index]@173:5:W vs. H[*maxPos]@177:15:W
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+#include <time.h>
+
+
+/*--------------------------------------------------------------------
+ * Constants
+ */
+#define PATH -1
+#define NONE 0
+#define UP 1
+#define LEFT 2
+#define DIAGONAL 3
+/* End of constants */
+
+/*--------------------------------------------------------------------
+* Helpers
+*/
+#define min(x, y) (((x) < (y)) ? (x) : (y))
+#define max(a,b) ((a) > (b) ? a : b)
+
+// #define DEBUG
+/* End of Helpers */
+
+/*--------------------------------------------------------------------
+ * Global Variables
+ */
+//Defines size of strings to be compared
+long long int m ; //Columns - Size of string a
+long long int n ;  //Lines - Size of string b
+
+//Defines scores
+int matchScore = 5;
+int missmatchScore = -3;
+int gapScore = -4;
+
+//Strings over the Alphabet Sigma
+char *a, *b;
+
+/* End of global variables */
+void generate() {
+    //Random seed
+    srand(time(NULL));
+
+    //Generates the values of a
+    long long int i;
+    for (i = 0; i < m; i++) {
+        int aux = rand() % 4;
+        if (aux == 0)
+            a[i] = 'A';
+        else if (aux == 2)
+            a[i] = 'C';
+        else if (aux == 3)
+            a[i] = 'G';
+        else
+            a[i] = 'T';
+    }
+
+    //Generates the values of b
+    for (i = 0; i < n; i++) {
+        int aux = rand() % 4;
+        if (aux == 0)
+            b[i] = 'A';
+        else if (aux == 2)
+            b[i] = 'C';
+        else if (aux == 3)
+            b[i] = 'G';
+        else
+            b[i] = 'T';
+    }
+} /* End of generate */
+
+
+/*--------------------------------------------------------------------
+ * Function:    nElement
+ * Purpose:     Calculate the number of i-diagonal elements
+ */
+long long int nElement(long long int i) {
+    if (i < m && i < n) {
+        //Number of elements in the diagonal is increasing
+        return i;
+    }
+    else if (i < max(m, n)) {
+        //Number of elements in the diagonal is stable
+        long int min = min(m, n);
+        return min - 1;
+    }
+    else {
+        //Number of elements in the diagonal is decreasing
+        long int min = min(m, n);
+        return 2 * min - i + abs(m - n) - 2;
+    }
+}
+
+/*--------------------------------------------------------------------
+ * Function:    matchMissmatchScore
+ * Purpose:     Similarity function on the alphabet for match/missmatch
+ */
+int matchMissmatchScore(long long int i, long long int j) {
+    if (a[j - 1] == b[i - 1])
+        return matchScore;
+    else
+        return missmatchScore;
+}  /* End of matchMissmatchScore */
+
+
+void similarityScore(long long int i, long long int j, int* H, int* P, long long int* maxPos) {
+
+    int up, left, diag;
+
+    //Stores index of element
+    long long int index = m * i + j;
+
+    //Get element above
+    up = H[index - m] + gapScore;
+
+    //Get element on the left
+    left = H[index - 1] + gapScore;
+
+    //Get element on the diagonal
+    diag = H[index - m - 1] + matchMissmatchScore(i, j);
+
+    //Calculates the maximum
+    int max = NONE;
+    int pred = NONE;
+    /* === Matrix ===
+     *      a[0] ... a[n]
+     * b[0]
+     * ...
+     * b[n]
+     *
+     * generate 'a' from 'b', if '←' insert e '↑' remove
+     * a=GAATTCA
+     * b=GACTT-A
+     *
+     * generate 'b' from 'a', if '←' insert e '↑' remove
+     * b=GACTT-A
+     * a=GAATTCA
+    */
+
+    if (diag > max) { //same letter ↖
+        max = diag;
+        pred = DIAGONAL;
+    }
+
+    if (up > max) { //remove letter ↑
+        max = up;
+        pred = UP;
+    }
+
+    if (left > max) { //insert letter ←
+        max = left;
+        pred = LEFT;
+    }
+    //Inserts the value in the similarity and predecessor matrixes
+    H[index] = max;
+    P[index] = pred;
+
+    //Updates maximum score to be used as seed on backtrack
+    if (max > H[*maxPos]) {        
+    #pragma omp critical
+        *maxPos = index;
+    }
+
+}  /* End of similarityScore */
+
+/*--------------------------------------------------------------------
+ * Function:    calcElement
+ * Purpose:     Calculate the position of (si, sj)-element
+ */
+void calcFirstDiagElement(long long int *i, long long int *si, long long int *sj) {
+    // Calculate the first element of diagonal
+    if (*i < n) {
+        *si = *i;
+        *sj = 1;
+    } else {
+        *si = n - 1;
+        *sj = *i - n + 2;
+    }
+}
+
+
+int main(int argc, char* argv[]) {
+    m = 2048;
+    n = 2048;
+
+#ifdef DEBUG
+    printf("\nMatrix[%lld][%lld]\n", n, m);
+#endif
+
+    //Allocates a and b
+    a = malloc(m * sizeof(char));
+    b = malloc(n * sizeof(char));
+
+    //Because now we have zeros
+    m++;
+    n++;
+
+    //Allocates similarity matrix H
+    int *H;
+    H = calloc(m * n, sizeof(int));
+
+    //Allocates predecessor matrix P
+    int *P;
+    P = calloc(m * n, sizeof(int));
+
+
+    //Gen rand arrays a and b
+    generate();
+
+
+    //Start position for backtrack
+    long long int maxPos = 0;
+    //Calculates the similarity matrix
+    long long int i, j;
+
+    //Gets Initial time
+    double initialTime = omp_get_wtime();
+
+    long long int si, sj, ai, aj;
+
+    //Because now we have zeros ((m-1) + (n-1) - 1)
+    long long int nDiag = m + n - 3;
+    long long int nEle;
+
+    #pragma omp parallel \
+    default(none) shared(H, P, maxPos, nDiag) private(nEle, i, si, sj, ai, aj)
+    {
+        for (i = 1; i <= nDiag; ++i)
+        {
+            nEle = nElement(i);
+            calcFirstDiagElement(&i, &si, &sj);
+            #pragma omp for
+            for (j = 1; j <= nEle; ++j)
+            {
+                ai = si - j + 1;
+                aj = sj + j - 1;
+                similarityScore(ai, aj, H, P, &maxPos);
+            }
+        }
+    }
+}

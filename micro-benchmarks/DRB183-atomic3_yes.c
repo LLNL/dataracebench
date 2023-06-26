1b0251fc751d627128f00a32407e5f732dd6b92f
LChenGit <lechen@iastate.edu>
2023-06-26 15:03:51 -0700

    updated contributor info; data race info.

--- micro-benchmarks/DRB183-atomic3_yes.c
+++ micro-benchmarks/DRB183-atomic3_yes.c
@@ -1,22 +1,77 @@
-/* Race because the write to s is not protected by atomic */
+/*
+Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+Produced at the Lawrence Livermore National Laboratory
+Written by Chunhua Liao, Pei-Hung Lin, Joshua Asplund,
+Markus Schordan, and Ian Karlin
+(email: liao6@llnl.gov, lin32@llnl.gov, asplund1@llnl.gov,
+schordan1@llnl.gov, karlin1@llnl.gov)
+LLNL-CODE-732144
+All rights reserved.
+
+This file is part of DataRaceBench. For details, see
+https://github.com/LLNL/dataracebench. Please also see the LICENSE file
+for our additional BSD notice.
+
+Redistribution and use in source and binary forms, with
+or without modification, are permitted provided that the following
+conditions are met:
+
+* Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the disclaimer below.
+
+* Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the disclaimer (as noted below)
+  in the documentation and/or other materials provided with the
+  distribution.
+
+* Neither the name of the LLNS/LLNL nor the names of its contributors
+  may be used to endorse or promote products derived from this
+  software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL LAWRENCE LIVERMORE NATIONAL
+SECURITY, LLC, THE U.S. DEPARTMENT OF ENERGY OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
 #include <stdio.h>
-int main() {
-  int x=0, s=0;
-#pragma omp parallel sections shared(x,s) num_threads(2)
+/*
+ * This is a program based on a dataset contributed by 
+ * Wenhao Wu and Stephen F. Siegel @Univ. of Delaware.
+ 
+ * Race because the write to s is not protected by atomic
+ * Data race pair: s@64:7:W vs. s@72:16:R
+ */
+int main()
+{
+  int x = 0, s = 0;
+#pragma omp parallel sections shared(x, s) num_threads(2)
   {
 #pragma omp section
     {
-      x=1;
-      s=1;
+      x = 1;
+      s = 1;
     }
 #pragma omp section
     {
       int done = 0;
-      while (!done) {
+      while (!done)
+      {
 #pragma omp atomic read seq_cst
-	done = s;
+        done = s;
       }
-      x=2;
+      x = 2;
     }
   }
   printf("%d\n", x);
